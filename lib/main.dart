79e798d7c41f48c285f8727c2b48ba32e52aca2a
Deborah_Maia2 <48deborahmaia@gmail.com>
2025-01-18 01:43:23 +0000

    Mudanças visuais

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _Home2State();
+  State<Home> createState() => _HomeState();
 }
 
-class _Home2State extends State<Home> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,25 +23,47 @@ class _Home2State extends State<Home> {
         title: Text("Water Day"),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
       ),
-      body: Column(
-        children: [
-          Text(
-            "Quantos litros de água por dia devemos beber",
-            style: TextStyle(
-              color: Colors.pinkAccent,
-              fontSize: 15,
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            Text(
+              "Quantos litros de água por dia devemos beber",
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                fontSize: 15,
+              ),
+              textAlign: TextAlign.justify,
             ),
-            textAlign: TextAlign.justify,
-          ),
-          Form(
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Digite seu peso",
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Digite seu peso",
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Botão pressionado");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pinkAccent,
+                      ),
+                      child: Text(
+                        "Enviar",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
