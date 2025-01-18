b51dc61c0db7407c02a44109f3422beff6add297
Deborah_Maia2 <48deborahmaia@gmail.com>
2025-01-18 00:53:14 +0000

    Criação página principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,47 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _Home2State();
+}
+
+class _Home2State extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Colors.pinkAccent,
+        title: Text("Water Day"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Column(
+        children: [
+          Text(
+            "Quantos litros de água por dia devemos beber",
+            style: TextStyle(
+              color: Colors.pinkAccent,
+              fontSize: 15,
+            ),
+            textAlign: TextAlign.justify,
+          ),
+          Form(
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Digite seu peso",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
